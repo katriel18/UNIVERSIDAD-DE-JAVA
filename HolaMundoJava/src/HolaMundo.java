979bc10194903603b89062e6384927d0cc9b0170
katriel <katriel181920@gmail.com>
2020-10-04 14:45:20 -0500

    inferencia de tipos

--- HolaMundoJava/src/HolaMundo.java
+++ HolaMundoJava/src/HolaMundo.java
@@ -1,10 +1,10 @@
 
 public class HolaMundo {
     public static void main(String args[]){
-        
+        //definicion de la variable
         int variableEntera=20;
         System.out.println(variableEntera);
-        
+        //modificacion del valor de la variable
         variableEntera=18;
         System.out.println(variableEntera);//sout + tab
         
@@ -14,5 +14,12 @@ public class HolaMundo {
          varibleCadena="adios";
         System.out.println(varibleCadena);//ctrl + space
         
+        //var - inferencia de tipo
+        var variableGeneral1=2;
+        System.out.println(variableGeneral1);//sout + tab -> ctrl + space
+        
+        var variableGeneral2="dos";
+        System.out.println("variableGeneral2 = " + variableGeneral2);//coutv + space
+         
     }
 }
