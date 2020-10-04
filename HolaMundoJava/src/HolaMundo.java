5e2942390a40d1d1cbea960e923e5b4dce0fdbfb
katriel <katriel181920@gmail.com>
2020-10-04 14:29:59 -0500

    variables - tipo entero - tipo String

--- HolaMundoJava/src/HolaMundo.java
+++ HolaMundoJava/src/HolaMundo.java
@@ -1,6 +1,18 @@
 
 public class HolaMundo {
     public static void main(String args[]){
-        System.out.println("Hola mundo desde Java");
+        
+        int variableEntera=20;
+        System.out.println(variableEntera);
+        
+        variableEntera=18;
+        System.out.println(variableEntera);//sout + tab
+        
+        String varibleCadena="hola";
+        System.out.println(varibleCadena);//ctrl + space
+        
+         varibleCadena="adios";
+        System.out.println(varibleCadena);//ctrl + space
+        
     }
 }
