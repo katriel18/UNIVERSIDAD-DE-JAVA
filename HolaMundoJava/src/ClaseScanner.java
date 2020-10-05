110fc7c67760ce99556081a070d41517a5fea5f3
katriel <katriel181920@gmail.com>
2020-10-05 11:16:48 -0500

    clase scanner

---
+++ HolaMundoJava/src/ClaseScanner.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class ClaseScanner {
+     public static void main(String args[]) {
+         var usuario="CARLOS";
+         System.out.println("usuario = " + usuario);
+         
+         Scanner consola=new Scanner(System.in);
+         
+         System.out.println("Escribe tu nombre:");
+         usuario=consola.nextLine();
+         System.out.println("usuario = " + usuario);
+     }
+}
