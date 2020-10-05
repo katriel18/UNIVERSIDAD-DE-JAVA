7645f0af1b299f38830cd05cc4ff9a3d177ae50d
katriel <katriel181920@gmail.com>
2020-10-05 12:10:02 -0500

    comentarios

---
+++ HolaMundoJava/src/TiposPrimitivos.java
@@ -0,0 +1,11 @@
+
+public class TiposPrimitivos {
+
+    public static void main(String args[]) {
+        //comentarios de una linea
+        /*
+    comnentarios de varias lineas
+         */
+    }
+
+}
