f84447a683011eb7508ce45fefdbe5fc44e80aae
katriel <katriel181920@gmail.com>
2020-10-14 10:17:52 -0500

    ejercicio1 if else

---
+++ HolaMundoJava/src/EjercicioIfElse.java
@@ -0,0 +1,13 @@
+
+public class EjercicioIfElse {
+   public static void main(String args[]) {
+        var numero = 2;
+        var texto="Desconocido";
+        if (numero==1) {
+            texto = "uno"; 
+        } else if(numero==2) {
+            texto = "dos" ;
+        }
+        System.out.println("texto = " + texto);
+    } 
+}
