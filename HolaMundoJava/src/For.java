454a3a7532ac54e82481e2eff552a9505cd7d1b3
katriel <katriel181920@gmail.com>
2020-10-14 11:42:43 -0500

    for

---
+++ HolaMundoJava/src/For.java
@@ -0,0 +1,12 @@
+
+public class For {
+
+    public static void main(String[] args) {
+        
+        for(var contador=0;contador<3;contador++){
+            System.out.println("contador = " + contador);
+        }
+        
+    }
+    
+}
