05a9512ea48df9fd94dfdd40983ae494a4217ab6
katriel <katriel181920@gmail.com>
2020-10-14 11:42:30 -0500

    do while

---
+++ HolaMundoJava/src/DoWhile.java
@@ -0,0 +1,17 @@
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        var contador = 0;
+        
+        do {
+            
+            System.out.println("contador = " + contador);
+            contador++;
+            
+        } while (contador < 3);
+        
+    }
+
+}
