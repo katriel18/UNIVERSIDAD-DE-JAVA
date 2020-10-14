559f256c46cd5e5594d153c6ce1c762e9161b68c
katriel <katriel181920@gmail.com>
2020-10-14 11:11:49 -0500

    while

---
+++ HolaMundoJava/src/While.java
@@ -0,0 +1,14 @@
+
+public class While {
+
+    public static void main(String[] args) {
+        
+        var contador = 0;
+        while (contador < 3) {
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+        
+    }
+    
+}
