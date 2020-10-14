3d7ce891f0f1a5c31620147e97e868e66e73a288
katriel <katriel181920@gmail.com>
2020-10-14 09:59:16 -0500

    sentencia if else

---
+++ HolaMundoJava/src/IfElse.java
@@ -0,0 +1,13 @@
+
+public class IfElse {
+
+    public static void main(String args[]) {
+        var condicion = false;
+        if (condicion) {
+            System.out.println("condicion = " + condicion);
+        } else {
+            System.out.println("condicion = " + condicion);
+        }
+    }
+
+}
