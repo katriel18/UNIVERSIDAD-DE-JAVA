3d98869be7331249bc83839306fa4e2558ce6662
katriel <katriel181920@gmail.com>
2020-10-25 11:07:44 -0500

    enumeraciones con variables y constructor

---
+++ ProgramacionJava/src/enumeraciones/ejemplo1/Continentes.java
@@ -0,0 +1,21 @@
+package enumeraciones.ejemplo1;
+
+public enum Continentes {
+
+    AFRICA(55),
+    EUROPA(44),
+    ASIA(33),
+    AMERICA(22),
+    OCEANIA(11);
+
+    private final int paises;
+
+    Continentes(int paises) {
+        this.paises = paises;
+    }
+
+    public int getPaises() {
+        return paises;
+    }
+    
+}
---
+++ ProgramacionJava/src/enumeraciones/ejemplo1/Test.java
@@ -0,0 +1,16 @@
+
+package enumeraciones.ejemplo1;
+
+public class Test {
+
+    public static void main(String[] args) {
+       
+        System.out.println(Continentes.AFRICA);
+        System.out.println("paises: " +Continentes.AFRICA.getPaises());
+     
+        System.out.println(Continentes.AMERICA);
+        System.out.println("paises: " +Continentes.AMERICA.getPaises());
+      
+    }
+  
+}
