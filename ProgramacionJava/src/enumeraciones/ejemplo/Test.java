8e8797e3976f184d7772ef88a41a857a109e0ea4
Osti Katriel <katriel181920@gmail.com>
2020-10-26 09:08:23 -0500

    enumeraciones editado

---
+++ ProgramacionJava/src/enumeraciones/ejemplo/Test.java
@@ -0,0 +1,16 @@
+
+package enumeraciones.ejemplo;
+
+public class Test {
+
+    public static void main(String[] args) {
+       
+        System.out.println(Continentes.AFRICA);
+        System.out.println("paises: " +Continentes.AFRICA.getPaises());
+     
+        System.out.println(Continentes.AMERICA);
+        System.out.println("paises: " +Continentes.AMERICA.getPaises());
+      
+    }
+  
+}
