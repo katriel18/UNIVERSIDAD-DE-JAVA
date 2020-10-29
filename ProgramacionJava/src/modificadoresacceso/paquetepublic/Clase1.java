28f5731f43cef4a1ee61bfb44afbfbdb89fc7ddc
Osti Katriel <katriel181920@gmail.com>
2020-10-28 22:20:49 -0500

    public

---
+++ ProgramacionJava/src/modificadoresacceso/paquetepublic/Clase1.java
@@ -0,0 +1,19 @@
+
+package modificadoresacceso.paquetepublic;
+
+public class Clase1 {
+    
+    public String atributoPublico="atributo Publico";
+
+    public Clase1() {
+        System.out.println("constructor Publico");
+    }
+
+    public void metodoPublico() {
+        System.out.println("metodo Publico");
+    }
+    
+    
+    
+    
+}
---
+++ ProgramacionJava/src/testpublic/main.java
@@ -0,0 +1,16 @@
+
+package testpublic;
+
+import modificadoresacceso.paquetepublic.Clase1;
+
+public class main {
+
+    public static void main(String[] args) {
+      
+        Clase1 c=new Clase1();
+        System.out.println(c.atributoPublico);
+        c.metodoPublico();
+        
+    }
+    
+}
