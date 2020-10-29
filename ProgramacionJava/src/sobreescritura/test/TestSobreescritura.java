6e6c7b4c7f0b15bf076a5a2e51c21afa23ce8213
Osti Katriel <katriel181920@gmail.com>
2020-10-28 23:35:45 -0500

    sobreescritura

---
+++ ProgramacionJava/src/sobreescritura/test/TestSobreescritura.java
@@ -0,0 +1,11 @@
+package sobreescritura.test;
+
+import sobreescritura.domain.Gerente;
+
+
+public class TestSobreescritura {
+    public static void main(String[] args) {
+        Gerente gerente1 = new Gerente("Juan", 5000, "Sistemas");
+        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+}
