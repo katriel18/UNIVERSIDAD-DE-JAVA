e86ba170e8e4bb6533e55bc8a506890127bc7221
Osti Katriel <katriel181920@gmail.com>
2021-02-18 17:19:52 -0500

    Manejo de cookies con servlets

---
+++ ManejoCookies/src/main/java/web/CookiesServlet.java
@@ -0,0 +1,44 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/CookiesServlet")
+public class CookiesServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        //suponemos que el usuario visita por primera vez nuestro sitio
+        boolean nuevoUsuario = true;
+        
+        //Obtenemos el arreglo de cookies
+        Cookie[] cookies = request.getCookies();
+        
+        //buscamos si ya existe una cookie creada con anterioridad
+        //llamada visitanteRecurrente
+        if(cookies != null){
+            for(Cookie c: cookies){
+                if(c.getName().equals("visitanteRecurrente") && c.getValue().equals("si")){
+                    //si ya existe la cookie es un usuario recurrente
+                    nuevoUsuario = false;
+                    break;
+                }
+            }
+        }
+        
+        String mensaje = null;
+        if(nuevoUsuario){
+            Cookie visitanteCookie = new Cookie("visitanteRecurrente","si");
+            response.addCookie(visitanteCookie);
+            mensaje = "Gracias por visitar nuestro sitio por primera vez";
+        }
+        else{
+            mensaje = "Gracias por visitar NUEVAMENTE nuestro sitio";
+        }
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("Mensaje:" + mensaje);
+        out.close();
+    }
+}
